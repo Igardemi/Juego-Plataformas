0c471ae62d6fb061b282cc832b2b2cf03abf220c
Ivan <igardemi@hotmail.es>
2021-12-02 20:01:50 +0100

    Sesion 2 Ejercicio 3 Clic en la carta y muestra su nombre por consola.

---
+++ Sesion 2/ejercicio 3/Assets/Script/CardScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+          
+
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("Has hecho click en la carta " + name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
