bf88fb6c1eea1c556ec3e25c069b29331a5c2e9d
Ivan <igardemi@hotmail.es>
2021-12-10 17:43:01 +0100

    sesion 3 Ejercicio 3

---
+++ Sesion 3/ejercicio3/Assets/Scripts/CardScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    SpriteRenderer myCard;
+    public Sprite front;
+    public Sprite back;
+    bool faceUp = false;
+
+    private void Awake()
+    {
+        myCard = GetComponent<SpriteRenderer>();
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCard.sprite = back;
+       
+
+
+    }
+    private void OnMouseDown()
+    {
+        if (faceUp == false)
+        {
+            GetComponent<SpriteRenderer>().sprite = front;
+            faceUp = true;
+            
+        }
+        else {
+            GetComponent<SpriteRenderer>().sprite = back;
+            faceUp = false;
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
