659da201ce1182a92465bf6e3ece5bd20c2c68b0
Ivan <igardemi@hotmail.es>
2022-01-16 07:04:41 +0100

    Sesion 5 Simulador Space invaders Actividad 1

---
+++ Sesion 5/Space invanders ejercicio 1/Scripts/RocketScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyObject(this.gameObject, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
