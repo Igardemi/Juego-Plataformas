357760700fd5a003f332df2c4d0ce22e923f5584
Ivan <igardemi@hotmail.es>
2022-01-22 20:15:24 +0100

    Sesion 6 ejercicio 2.- Haz que sólo se pueda disparar una bala a la vez. Es decir, que sólo aparezca una bala en la pantalla.

---
+++ Sesion 6/Space invanders ejercicio 2/Scripts/SpaceshipScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipScript : MonoBehaviour
+{
+    public int force;
+    Rigidbody2D myRigidBody;
+    public GameObject rocket;
+    bool shoot = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //capturamos el movimiento de las teclas left y right
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+
+        //traferimos el movimiento a nuestro objeto nave a través de añadir velocidad.
+        myRigidBody.velocity = transform.right * movimientoHorizontal * force;
+
+        //Restringimos el valor en x para que la nave no salga de la pantalla
+        float xPos = Mathf.Clamp(myRigidBody.position.x, -6.5f, 6.5f);
+       
+        myRigidBody.position = new Vector2(xPos, transform.position.y);
+
+        if (Input.GetButton("Jump") && shoot==true)//capturamos el pulsado de la tecla espacio.
+        {
+            ShootSystem();
+
+        }
+
+    }
+        private void ShootSystem()
+        {
+
+                //Instanciamos el prefab del misil.
+                Vector2 posicionMisil = new Vector2(transform.position.x, transform.position.y + 1.8f);
+                GameObject clone = Instantiate(rocket, posicionMisil, Quaternion.identity);
+
+                //le otorgamos fuerza al rocket para su movimiento.
+                Rigidbody2D cloneRB = clone.GetComponent<Rigidbody2D>();
+                //definimos la direccion de movimiento.
+                Vector2 direccion = new Vector2(0f, 1f);
+                //añadimos la fuerzaal rocket.
+                int fuerzaRocket = 20;
+                //añadimos la fuerza para el movimiento.
+                cloneRB.velocity = direccion * fuerzaRocket;
+                shoot = false;
+
+        }
+
+}
