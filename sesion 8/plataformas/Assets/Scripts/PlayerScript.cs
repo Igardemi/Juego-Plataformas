b8aa401d034e58f41e062ee291c05cf60973f216
Ivan <igardemi@hotmail.es>
2022-02-01 02:02:31 +0100

    sesion 8 ejercicio 5 cambiar de sentido según movimiento

--- sesion 8/plataformas/Assets/Scripts/PlayerScript.cs
+++ sesion 8/plataformas/Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,8 @@ public class PlayerScript : MonoBehaviour
 {
     Rigidbody2D playerRB;
     float speed = 5;
+    float move;
+    bool lookRight = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +17,31 @@ public class PlayerScript : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        float move = Input.GetAxis("Horizontal");
-        //le damos speed a velocity para que realice el movimiento, en el eje y no varia.
-        playerRB.velocity = new Vector2(move * speed, playerRB.velocity.y);
+    { 
+        Movements();        
+    }
 
+    private void Movements() {
 
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            move = Input.GetAxis("Horizontal");
+            //le damos speed a velocity para que realice el movimiento, en el eje y no varia.
+            playerRB.velocity = new Vector2(move * speed, playerRB.velocity.y);
+            lookRight = true;
+        }
+        else if (Input.GetAxis("Horizontal") == 0) { 
+        //mantiene la posicion.
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            move = Input.GetAxis("Horizontal");
+            //le damos speed a velocity para que realice el movimiento, en el eje y no varia.
+            playerRB.velocity = new Vector2(move * speed, playerRB.velocity.y);
+            lookRight = false;
+        }
 
-        
     }
 }
