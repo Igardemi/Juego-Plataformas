faf32edc463098a8e540efa91158450c2fffc85e
Ivan <igardemi@hotmail.es>
2022-02-01 02:01:39 +0100

    sesion 8 ejercicio 4 añadir movimiento a player

---
+++ sesion 8/plataformas/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D playerRB;
+    float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        //le damos speed a velocity para que realice el movimiento, en el eje y no varia.
+        playerRB.velocity = new Vector2(move * speed, playerRB.velocity.y);
+
+
+
+        
+    }
+}
