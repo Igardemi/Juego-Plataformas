93a0d645ad01de4fcc84cf10d21ccc93532d6919
Ivan <igardemi@hotmail.es>
2022-02-25 02:34:28 +0100

    Sesion 10 actividad 10 realizar el salto del personaje con animación incluida

--- sesion 8/plataformas/Assets/Scripts/PlayerScript.cs
+++ sesion 8/plataformas/Assets/Scripts/PlayerScript.cs
@@ -7,20 +7,47 @@ public class PlayerScript : MonoBehaviour
     Rigidbody2D playerRB;
     float speed = 5;
     float move;
+    public float forceJump= 10;
     bool lookRight = true;
     SpriteRenderer myRender;
     Animator animatorSetting;//definimos componente animator para pasarle las variables de las conmdiciones
+    bool isGrounded = true;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
         myRender = GetComponent<SpriteRenderer>();
         animatorSetting = gameObject.GetComponent<Animator>();
+        //para que no se detecte asi mismo
+        Physics2D.queriesStartInColliders = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    { 
+    {
+        animatorSetting.SetFloat("VerticalVelocity", playerRB.velocity.y);
+        Debug.Log("Velocidad en y " + playerRB.velocity.y);
+
+        
+        if (Input.GetAxis("Jump") > 0 && isGrounded) {
+            Debug.Log("hemos pulsado el salto");
+            playerRB.velocity = new Vector2(playerRB.velocity.x, 0);
+            playerRB.AddForce(new Vector2(0,forceJump),ForceMode2D.Impulse);
+            isGrounded = false; //despega del suelo
+            animatorSetting.SetBool("IsGrounded", false);
+        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+        // If it hits something...
+        if (hit.collider != null && isGrounded==false && playerRB.velocity.y < 0 )
+        {
+            if (hit.distance < 0.4)
+            {
+                Debug.Log("Detecto un objeto " + hit.collider.gameObject.name +". A discancia: "+ hit.distance);
+                isGrounded = true;
+                animatorSetting.SetBool("IsGrounded",true);
+            }
+        }
+       
         Movements();        
     }
 
@@ -42,7 +69,7 @@ public class PlayerScript : MonoBehaviour
 
         //le damos speed a velocity para que realice el movimiento, en el eje y no varia.
         playerRB.velocity = new Vector2(move * speed, playerRB.velocity.y);
-        Debug.Log("move: "+move);
+        //Debug.Log("move: "+ move);
         animatorSetting.SetFloat("MoveSpeed", Mathf.Abs(move));
     }
 
@@ -51,4 +78,8 @@ public class PlayerScript : MonoBehaviour
        myRender.flipX = !myRender.flipX;
        lookRight = !lookRight;      
     }
+
+    //raycast, emite una linea laser para detectar la distancia a los demás objetos.
+
+
 }
